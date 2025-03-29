ae5c1e51c95eb4ce0d4b0b5b784f8513e36f5db0
mjaynes05 <mjaynes05@icloud.com>
2025-03-29 11:14:17 -0500

    extract_text func

--- src/entity.py
+++ src/entity.py
@@ -2,3 +2,18 @@ from notion_client import Client
 notion = Client(auth="API_KEY")
 page_id = "557885410ce143b0a5305ed6cd46572e"
 page_content = notion.blocks.children.list(page_id)
+def extract_text(blocks):
+    """Extracts plain text from Notion blocks""""
+    try:
+        if 'results' not in blocks:
+            return ""
+        return ''.join(
+            t['plain_text']
+            for block in blocks['results']
+            if block.get('type') == 'paragraph' and 'text' in block['paragraph']
+            for t in block['paragraph'].get['text',[]]
+        )
+    except Exception as e:
+        print(f"Error inside extract_text: {e}")
+        return ""
+
