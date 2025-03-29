bff40bbfc0ec88251d04385f09390b46137f3fea
mjaynes05 <mjaynes05@icloud.com>
2025-03-29 12:00:36 -0500

    fixed errors

--- src/entity.py
+++ src/entity.py
@@ -10,18 +10,23 @@ def extract_text(blocks):
         return ''.join(
             t['plain_text']
             for block in blocks['results']
-            if block.get('type') == 'paragraph' and 'text' in block['paragraph']
+            if block.get('type') == 'paragraph'
+            for t in block['paragraph'].get('text',[])
         )
     except Exception as e:
         print(f"Error inside extract_text: {e}")
-        return ''
+        return ""
 
 client = openai.OpenAI(
     base_url="https://api.groq.com/openai/v1",
     api_key=os.environ.get("GROQ_KEY")
 )
-def summarize(text):
-    text = extract_text(page_id)
+def summarize(page_id):
+    """Summarizes extracted text using OpenAI"""
+    page_content = notion.blocks.children.list(page_id) #Fetch Notion content
+    text = extract_text(page_id) #Extract text from Notion blocks
+    if not text.strip():
+        return "No text found to summarize."
     try:
         response = client.chat.completion.create(
             model = 'llama-3.3-70b-versatile',
@@ -29,7 +34,7 @@ def summarize(text):
                     {"role": "system", "content": "You are a helpful quiz question generator."},
                     {"role": "user", "content": f'Summarize this text: {text}'},
                 ]
-            return response.choices[0].message.content.strip()
         )
+        return response.choices[0].message.content.strip()
     except Exception as e:
         return f'error generating question {e}'
